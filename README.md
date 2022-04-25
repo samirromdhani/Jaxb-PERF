5f983a42e86f3e0c18f27a4a734d29d943a0f811
Samir Romdhani <samir.romdhani@rte-france.com>
2022-04-25 12:36:46 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Jaxb-PERF
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Notes](#notes)
+* [Performance Test Report](#performance-test-report)
+
+## General info
+**Evaluating jaxb performance (jaxb2) by comparing it with others technologies**
+
+## Technologies
+### Java API
+* JAXB: default
+* JAXB: with SAX
+### CoMPAS SCT Commons
+* JAXB: MarshallerWrapper
+### Spring Feature
+* JAXB2: MarshallerJaxb2Wrapper (current) 
+### Others
+* JAXB-STREAM 
+* JIBX 
+
+## Notes
+
+_**MarshallerWrapper**_  : CoMPAS SCT Commons Implementation -> unmarshalDefault (javax.xml.bind.*)
+
+_**GoodJAXBUtilGeneric**_ : unmarshalWithSAX ->  unmarshalDefault (javax.xml.bind.*)
+
+_**MarshallerJaxb2Wrapper**_  : Jaxb2Marshaller (org.springframework.oxm.jaxb.Jaxb2Marshaller)
+
+## Performance Test Report
+
+Type / File size | 7 MB | 70 MB
+--- | --- | ---
+*JAXB2* (/v0) | ~2.2 s | ~22.5 s
+*JAXB with SAX* (/v1) | ~300 ms | ~2 s
+*JAXB CoMPAS sct* | TODO | TODO
---
+++ src/main/java/com/github/samirromdhani/jaxblib/rest/JAXBTest.java
@@ -0,0 +1,44 @@
+package com.github.samirromdhani.jaxblib.rest;
+
+import com.github.samirromdhani.jaxblib.commons.jaxb.GoodJAXBUtilGeneric;
+import com.github.samirromdhani.jaxblib.commons.jaxb2.MarshallerJaxb2Wrapper;
+import lombok.extern.java.Log;
+import org.lfenergy.compas.scl2007b4.model.SCL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@Log
+public class JAXBTest {
+
+    private static final String BIG_FILE_BASIC = "PERF/basic-7MB.xml";
+    private static final String BIG_FILE_M_10 = "PERF/m10-70MB.xml";
+
+    @Autowired
+    private MarshallerJaxb2Wrapper marshallerJaxb2Wrapper;
+    @Autowired
+    private GoodJAXBUtilGeneric goodJAXBUtilGeneric;
+
+    @GetMapping("/v0/ieds")
+    public List<String> testv0() {
+        InputStream xmlStream = getClass().getResourceAsStream("/" + BIG_FILE_M_10);
+        SCL scl = marshallerJaxb2Wrapper.unmarshall(xmlStream);
+        List<String> list = new ArrayList<>();
+        scl.getIED().forEach(tied -> list.add(tied.getName()));
+        return list;
+    }
+
+    @GetMapping("/v1/ieds")
+    public List<String> testv1() {
+        InputStream xmlStream = getClass().getResourceAsStream("/" + BIG_FILE_M_10);
+        SCL scl = goodJAXBUtilGeneric.unmarshal(SCL.class, xmlStream);
+        List<String> list = new ArrayList<>();
+        scl.getIED().forEach(tied -> list.add(tied.getName()));
+        return list;
+    }
+}
